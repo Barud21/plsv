4d345f64e539bb8c958d00d6510c602014a2e3f9
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-01-28 20:24:58 +0100

    Adding space every 3 digits of phone number

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 "use strict";
 
 const btnPickUp = document.querySelector(".btn--pick-up");
+const phoneNumber = document.getElementById("phone-number");
 
 btnPickUp.addEventListener("click", () => {
   console.log("Kliknięto przycisk");
+});
+
+phoneNumber.addEventListener("keydown", function (e) {
+  const txt = this.value;
+  if ((txt.length == 3 || txt.length == 7) && e.which !== 8) {
+    this.value = this.value + " ";
+  }
 });
