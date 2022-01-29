560817ca80c340bab706e6c156bcee70b8563845
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-01-29 11:51:09 +0100

    Actions for modal and modal buttons added

--- script.js
+++ script.js
@@ -2,8 +2,35 @@
 
 const btnStart = document.getElementById("btn-start");
 const btnPickUp = document.getElementById("btn-pick-up");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const btnEnd = document.getElementById("btn-end");
+const btnNextParcel = document.getElementById("btn-next-parcel");
 const phoneNumber = document.getElementById("phone-number");
 const form = document.querySelector(".form");
+const codeInput = document.getElementById("code");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const nextParcel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  codeInput.value = "";
+};
+
+const endPickUp = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  codeInput.value = "";
+  phoneNumber.value = "";
+  form.classList.add("hidden");
+  btnPickUp.classList.add("hidden");
+  btnStart.classList.remove("hidden");
+};
 
 btnStart.addEventListener("click", () => {
   console.log("Kliknięto przycisk");
@@ -18,3 +45,7 @@ phoneNumber.addEventListener("keydown", function (e) {
     this.value = this.value + " ";
   }
 });
+
+btnPickUp.addEventListener("click", openModal);
+btnNextParcel.addEventListener("click", nextParcel);
+btnEnd.addEventListener("click", endPickUp);
