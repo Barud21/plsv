803c4f7c2e7cf6d1be5c748b68e374391cac7f06
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2022-01-29 23:17:48 +0100

    Phone number and code validation changed

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const btnsOpenModal = document.querySelectorAll(".show-modal");
 const btnEnd = document.getElementById("btn-end");
 const btnNextParcel = document.getElementById("btn-next-parcel");
 const phoneNumber = document.getElementById("phone-number");
+const phoneNumberMsg = document.getElementById("phone-msg");
 const form = document.querySelector(".form");
 const codeInput = document.getElementById("code");
+const codeMsg = document.getElementById("code-msg");
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -20,6 +22,8 @@ const nextParcel = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
   codeInput.value = "";
+  codeMsg.classList.remove("hidden");
+  disablePickUp();
 };
 
 const endPickUp = function () {
@@ -30,6 +34,54 @@ const endPickUp = function () {
   form.classList.add("hidden");
   btnPickUp.classList.add("hidden");
   btnStart.classList.remove("hidden");
+  codeMsg.classList.remove("hidden");
+  phoneNumberMsg.classList.remove("hidden");
+  disablePickUp();
+};
+
+const enablePickUp = function () {
+  btnPickUp.disabled = false;
+  btnPickUp.classList.remove("btn--inactive");
+};
+
+const disablePickUp = function () {
+  btnPickUp.disabled = true;
+  btnPickUp.classList.add("btn--inactive");
+};
+
+const phoneValidation = function (phoneNumber) {
+  const number = phoneNumber.value;
+  let isPhoneNumber = /^\d+$/.test(number.replaceAll(" ", ""));
+  if (phoneNumber.value.length === 11 && isPhoneNumber) {
+    phoneNumberMsg.classList.add("hidden");
+    return true;
+  } else {
+    phoneNumberMsg.classList.remove("hidden");
+    return false;
+  }
+};
+
+const codeValidation = function (code) {
+  const pickUpCode = code.value;
+  let isPickUpCode = /^\d+$/.test(pickUpCode.replaceAll(" ", ""));
+  if (code.value.length === 4 && isPickUpCode) {
+    codeMsg.classList.add("hidden");
+    return true;
+  } else {
+    codeMsg.classList.remove("hidden");
+    return false;
+  }
+};
+
+const inputValidation = function (phoneNumber, code) {
+  const number = phoneValidation(phoneNumber);
+  const pickUpCode = codeValidation(code);
+
+  if (number && pickUpCode) {
+    enablePickUp();
+  } else {
+    disablePickUp();
+  }
 };
 
 btnStart.addEventListener("click", () => {
@@ -39,13 +91,47 @@ btnStart.addEventListener("click", () => {
   btnStart.classList.add("hidden");
 });
 
-phoneNumber.addEventListener("keydown", function (e) {
-  const txt = this.value;
-  if ((txt.length == 3 || txt.length == 7) && e.which !== 8) {
-    this.value = this.value + " ";
-  }
+phoneNumber.addEventListener("input", function (e) {
+  let target = e.target;
+  let position = target.selectionEnd;
+  length = target.value.length;
+  target.value = target.value
+    .replace(/[^\d]/g, "")
+    .replace(/(.{3})/g, "$1 ")
+    .trim();
+  target.selectionEnd = position +=
+    target.value.charAt(position - 1) === " " &&
+    target.value.charAt(length - 1) === " " &&
+    length !== target.value.length
+      ? 1
+      : 0;
 });
 
-btnPickUp.addEventListener("click", openModal);
+codeInput.addEventListener("input", function (e) {
+  let target = e.target;
+  let position = target.selectionEnd;
+  length = target.value.length;
+  target.value = target.value.replace(/[^\d]/g, "").trim();
+  target.selectionEnd = position +=
+    target.value.charAt(position - 1) === " " &&
+    target.value.charAt(length - 1) === " " &&
+    length !== target.value.length
+      ? 1
+      : 0;
+});
+
+btnPickUp.addEventListener("click", () => {
+  openModal();
+  inputValidation(phoneNumber, codeInput);
+});
+
 btnNextParcel.addEventListener("click", nextParcel);
 btnEnd.addEventListener("click", endPickUp);
+
+codeInput.addEventListener("keyup", function (e) {
+  inputValidation(phoneNumber, codeInput);
+});
+
+phoneNumber.addEventListener("keyup", function (e) {
+  inputValidation(phoneNumber, codeInput);
+});
